da4c9294fa63cce65bce5caf7d64ccd421cbcc16
Skitt2 <provaid@yandex.ru>
2022-05-07 15:19:05 +0300

    Add project files.

---
+++ RevitAPIExportNWC/Main.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIExportNWC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            var nwcOption = new NavisworksExportOptions();
+            doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "3dmodel.nwc", nwcOption);
+            return Result.Succeeded;
+            TaskDialog.Show("Экспорт", "Экспорт проведен удачно");
+        }
+    }
+}
